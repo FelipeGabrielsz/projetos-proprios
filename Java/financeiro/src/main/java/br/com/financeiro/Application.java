f4038e102cfa5be3c904b42447a505d93c9c7c5c
FelipeGabrielsz <fg.souza12@gmail.com>
2026-07-17 22:19:29 -0300

    :sparkles: feat: Criacao de packages

--- Java/financeiro/src/main/java/br/com/financeiro/Application.java
+++ Java/financeiro/src/main/java/br/com/financeiro/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinanceiroApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FinanceiroApplication.class, args);
+	static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
 	}
 
 }
